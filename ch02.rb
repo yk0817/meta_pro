e7836c2c2ea3b9e3f2391e0b974248ae6d3d7ab0
山本憲 <mi-numero@hotmail.co.jp>
2017-02-28 22:42:37 +0900

    テスト練習用

---
+++ ch02.rb
@@ -0,0 +1,16 @@
+require 'test/unit'
+
+def to_alphanumeric(s)
+  s.gsub(/[^\w\s]/,'')
+end
+
+class ToAlaphanumericTest < Testt::Unit::TestCase
+  #code
+end
+
+
+__END__
+
+testの参考 
+
+https://test-unit.github.io/ja/
