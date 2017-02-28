6ed3dfea3672ca276d43497bf0bcc1a5265490d6
山本憲 <mi-numero@hotmail.co.jp>
2017-02-28 23:47:22 +0900

    チュートリアルみたいなもの入力

--- ch02.rb
+++ ch02.rb
@@ -1,16 +1,27 @@
 require 'test/unit'
 
-def to_alphanumeric(s)
-  s.gsub(/[^\w\s]/,'')
+# def to_alphanumeric(s)
+#   s.gsub(/[^\w\s]/,'')
+# end
+
+class String
+  def to_alphanumeric
+    s.gsub(/[^\w\s]/,'')
+  end
 end
 
-class ToAlaphanumericTest < Testt::Unit::TestCase
-  #code
+class StringExtensionsTest < Test::Unit::TestCase
+  def test_strip_non_alphanumeric_characters
+    assert_equal('3 the Magic Number', '3 the Magic Number'.to_alphanumeric)
+  end
 end
 
 
 __END__
 
-testの参考 
+testの参考
 
 https://test-unit.github.io/ja/
+
+unit/testチュートリアルみたいなもの？
+http://d.hatena.ne.jp/zariganitosh/20080407/1207532468
