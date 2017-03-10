f9a97c58d51656194c89fb12aaf62de4a89a332e
山本憲 <mi-numero@hotmail.co.jp>
2017-03-10 23:10:39 +0900

    test

---
+++ ch02/instance_variable.rb
@@ -0,0 +1,21 @@
+class Test
+  def test2
+    @test2 = "test"
+  end
+end
+
+obj = Test.new
+p obj.instance_variables #
+
+obj2 = Test.new
+obj.test2 #@test2を定義
+p obj.instance_variables #
+
+class Test2
+  def initialize
+    @test2 = "test2"
+  end
+end
+
+obj3 = Test2.new
+p obj3.instance_variables #@test2
