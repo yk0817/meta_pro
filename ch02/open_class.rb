dc86a124fa3dbf067b0c69d03cbbb3439b75db67
山本憲 <mi-numero@hotmail.co.jp>
2017-03-10 22:54:55 +0900

    ch02 code example

---
+++ ch02/open_class.rb
@@ -0,0 +1,13 @@
+p "test".capitalize
+
+class String
+  def dokkiri!
+    return "!"
+  end
+  def capitalize
+    return self + "ss"
+  end
+end
+p "test".class
+p "test".dokkiri!
+p "test".capitalize
