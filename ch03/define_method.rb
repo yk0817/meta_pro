da07b90c76ddfff4fb5b46f8b0b1ec64738071c9
山本憲 <mi-numero@hotmail.co.jp>
2017-03-19 19:47:30 +0900

    test

--- ch03/define_method.rb
+++ ch03/define_method.rb
@@ -1,7 +1,12 @@
-class Greeting
-  {:cat => "meow", :dog => "bowbow"}.each do |name,message|
-    define_method(name) { message}
+class Siman
+  hash = [:test,:test2,:test3]
+  hash.each do |h|
+    define_method h do
+      puts h
+    end
   end
 end
 
-puts Greeting.new.dog
+s = Siman.new
+puts s.test
+
