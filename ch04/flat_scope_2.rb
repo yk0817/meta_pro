f2dc67ec66955c3916863075d991fd0742deec22
yk0817 <em072029@gmail.com>
2017-03-22 15:41:55 +0900

    test

--- ch04/flat_scope_2.rb
+++ ch04/flat_scope_2.rb
@@ -3,3 +3,9 @@ my_var = "成功"
 MyClass = Class.new do
   puts "クラス定義の中#{my_var}"
 end
+
+class MyClass2
+  puts "クラス定義の中#{my_var}"
+end
+
+p MyClass2.new # undefined local variable or method `my_var' for MyClass2:Class (NameError)
